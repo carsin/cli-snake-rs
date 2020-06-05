9c4cd6aee847103f1a034487a7e3cbc850ef2462
carsin <20868514+carsin@users.noreply.github.com>
2020-06-05 13:16:43 -1000

    general: update

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ use crossterm::{
 mod game;
 
 fn main() -> Result<()> {
-    stdout().execute(EnterAlternateScreen)?;
     terminal::enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
 
     let mut game = game::Game {
         width: 30,
@@ -24,6 +24,7 @@ fn main() -> Result<()> {
 
     game.render_map().expect("Failed to render game.");
 
+    // stdout().execute(LeaveAlternateScreen)?;
     terminal::disable_raw_mode()?;
     Ok(())
 }
